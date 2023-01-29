dc2a45ca252218005bebf77e52fa0dfab1c211aa
Kateryna Nanyzniuk <65312887+katerynananyzniuk@users.noreply.github.com>
2023-01-29 15:14:39 +0200

    fix: changed db link

--- src/axios/axios-quiz.js
+++ src/axios/axios-quiz.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://quiz-app-f417d-default-rtdb.europe-west1.firebasedatabase.app/'
+  baseURL: 'https://quiz-17f3a-default-rtdb.europe-west1.firebasedatabase.app/'
 })
